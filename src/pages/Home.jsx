f1f16551d05358c5bbb44f620db4c9f042ed36b0
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-03 15:01:33 +0300

    Create Home.jsx

---
+++ src/pages/Home.jsx
@@ -0,0 +1,34 @@
+ import { useEffect, useState } from 'react';
+import {fetchTrendingMovies} from '../services/fetchApi';
+import MoviesList from '../components/MoviesList/MoviesList';
+  
+ const Home = () => {
+    const [trendingMovies, setTrendingMovies] = useState([]);
+    const [error, setError] = useState([null]);
+
+        useEffect(() =>{
+            try {
+                const getTrendingMovies = async() => {
+                    const data = await fetchTrendingMovies()
+                    setTrendingMovies(data.results)
+                }
+                getTrendingMovies()
+                
+            } catch (error) {
+            setError(error)
+            }
+        }, [])
+
+        return(
+        <main>
+            <h2>Trendin movies</h2>
+
+            {trendingMovies && <MoviesList movies={trendingMovies} />}
+
+        </main>
+            )
+      
+  };
+
+  export default Home;
+  
