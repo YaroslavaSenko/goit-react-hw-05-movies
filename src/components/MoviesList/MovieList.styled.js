0ffe65644444364927604301bc2ab45097399d8f
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-03 15:00:33 +0300

    Create MovieList.styled.js

---
+++ src/components/MoviesList/MovieList.styled.js
@@ -0,0 +1,27 @@
+
+import styled from 'styled-components';
+
+
+
+export const Container = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fit, 300px);
+  gap: 16px;
+`;
+
+export const CardWrapper = styled.div`
+  border: 1px solid black;
+  border-radius: 4px;
+
+  > a {
+    text-decoration: none;
+  }
+`;
+
+export const MovieName = styled.h3`
+  padding: 4px;
+  margin-top: 8px;
+  margin-bottom: 0;
+  color: black;
+  text-align: center;
+`;
