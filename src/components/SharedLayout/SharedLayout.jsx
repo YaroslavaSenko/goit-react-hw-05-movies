713fac63cf9c9903419205efc70f45ddced0fb53
YaroslavaSenko <yaroslava_yatskevych@ukr.net>
2023-04-03 15:01:03 +0300

    Create SharedLayout.jsx

---
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -0,0 +1,33 @@
+import { Suspense } from 'react';
+import { Outlet } from 'react-router-dom';
+import { Container, Header, Logo, Link } from "./SharedLayout.styled";
+
+const SharedLayout = () => {
+  return (
+    <Container>
+      <Header>
+      <Logo>
+          <span role="img" aria-label="computer icon">
+            💻
+          </span>{" "}
+          Movie
+        </Logo>
+        <nav>
+          <Link to="/" end>
+            Home
+          </Link>
+          <Link to="/movies">Movies</Link>
+        </nav>
+      </Header>
+      <Suspense fallback={<div>Loading page...</div>}>
+        <Outlet />
+      </Suspense>
+    </Container>
+  );
+};
+
+export default SharedLayout;
+
+
+
+
